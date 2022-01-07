f4d3407cda33bba491e020495dd7e4ee344cd200
Andrew Culver <andrew.culver@gmail.com>
2022-01-06 17:01:52 -0800

    Initial commit.

---
+++ lib/minitest/just_failed.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "just_failed/version"
+
+module Minitest
+  module JustFailed
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "minitest/just_failed"
+
+require "minitest/autorun"
