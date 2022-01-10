4616f7b701630af9a881efb3d960c94d39a43b36
gazayas <g-zayas@hotmail.com>
2022-01-10 09:01:34 +0900

    Add necessary paths

--- lib/minitest/just_failed.rb
+++ lib/minitest/just_failed.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require_relative "just_failed/version"
 require "rails/railtie"
+require_relative "just_failed/test_unit_reporter"
+require_relative "composite_reporter"
 
 module Minitest
   module JustFailed
